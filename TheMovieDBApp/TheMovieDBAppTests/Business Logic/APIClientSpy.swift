ac562fe63d45381e6d8bf661b8c2e62408e932b6
Le0nX <cloudmindone@gmail.com>
2020-03-26 00:50:19 +0300

    fix: добавлены комментарии APIСlientSpy

--- TheMovieDBApp/TheMovieDBAppTests/Business Logic/APIClientSpy.swift
+++ TheMovieDBApp/TheMovieDBAppTests/Business Logic/APIClientSpy.swift
@@ -9,14 +9,23 @@
 import Foundation
 import TMDBNetwork
 
+/// Перехватчик запросов. Необходим для проверки асинхронного взаимодействия
+/// и вызова ожидаемого поведения в сервисах
 final class APIClientSpy: APIClient {
-        
-    private var messages = [(url: URLRequest, completion: Any)]()
+    
+    // MARK: - Public Properties
     
     var urlRequests: [URLRequest] {
         messages.map { $0.url }
     }
     
+    // MARK: - Private Properties
+    
+    private var messages = [(url: URLRequest, completion: Any)]()
+    
+    // MARK: - Public methods
+    
+    /// Мок метод запроса. Сохраняет замыкание и запрос
     func request<T>(_ endpoint: T,
                     completionHandler: @escaping (Result<T.Content, Error>) -> Void) -> Progress where T: Endpoint {
         guard let request = try? endpoint.makeRequest() else { return Progress() }
@@ -26,11 +35,19 @@ final class APIClientSpy: APIClient {
         return Progress()
     }
     
+    /// Мок метод вызова ожидаемой ошибки
+    /// - Parameter for: endpoint необходим для дедуцирования типа
+    /// - Parameter error: ожидаемая ошибка
+    /// - Parameter index: индекс из перехваченных вызовов
     func complete<T>(for: T, with error: Error, at index: Int = 0) where T: Endpoint {
         let complition = messages[index].completion as? (Result<T.Content, Error>) -> Void
         complition?(.failure(error))
     }
     
+    /// Мок метод передачи ожидаемых данных
+    /// - Parameter for: endpoint необходим для дедуцирования типа
+    /// - Parameter error: ожидаемая ошибка
+    /// - Parameter index: индекс из перехваченных вызовов
     func complete<T>(for: T, with data: T.Content, at index: Int = 0) where T: Endpoint {
         let complition = messages[index].completion as? (Result<T.Content, Error>) -> Void
         complition?(.success(data))
