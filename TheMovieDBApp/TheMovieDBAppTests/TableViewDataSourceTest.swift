87ababdf749f1cac16a63d80681af4f6e65cc1c8
Le0nX <cloudmindone@gmail.com>
2020-03-31 14:05:32 +0300

    feature: хелперы для добавления/удаления child'ов

---
+++ TheMovieDBApp/TheMovieDBAppTests/TableViewDataSourceTest.swift
@@ -0,0 +1,15 @@
+//
+//  TableViewDataSourceTest.swift
+//  TheMovieDBAppTests
+//
+//  Created by Denis Nefedov on 31.03.2020.
+//  Copyright © 2020 Den4ik's Team. All rights reserved.
+//
+
+import XCTest
+
+class TableViewDataSourceTest: XCTestCase {
+
+  
+
+}
