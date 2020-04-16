5841ff29a2820bdc1027818302307a9377336b03
Le0nX <cloudmindone@gmail.com>
2020-04-16 03:08:33 +0300

    fix: стили

--- TheMovieDBApp/TMDBNetwork/Client/Base/APIClient/Reachbility.swift
+++ TheMovieDBApp/TMDBNetwork/Client/Base/APIClient/Reachbility.swift
@@ -7,40 +7,31 @@
 //
 
 import Foundation
-import SystemConfiguration
-
-import Foundation
 import Network
 
-class NetworkReachability {
+/// Класс проверки соединения
+final class NetworkReachability {
 
-   var pathMonitor: NWPathMonitor!
-   var path: NWPath?
-   lazy var pathUpdateHandler: ((NWPath) -> Void) = { path in
-    self.path = path
-    if path.status == NWPath.Status.satisfied {
-        print("Connected")
-    } else if path.status == NWPath.Status.unsatisfied {
-        print("unsatisfied")
-    } else if path.status == NWPath.Status.requiresConnection {
-        print("requiresConnection")
+    var pathMonitor: NWPathMonitor!
+    var path: NWPath?
+    lazy var pathUpdateHandler: ((NWPath) -> Void) = { path in self.path = path }
+
+    let backgroudQueue = DispatchQueue.global(qos: .background)
+
+    init() {
+        pathMonitor = NWPathMonitor()
+        pathMonitor.pathUpdateHandler = self.pathUpdateHandler
+        pathMonitor.start(queue: backgroudQueue)
     }
-}
-
-let backgroudQueue = DispatchQueue.global(qos: .background)
-
-init() {
-    pathMonitor = NWPathMonitor()
-    pathMonitor.pathUpdateHandler = self.pathUpdateHandler
-    pathMonitor.start(queue: backgroudQueue)
-   }
-
- func isNetworkAvailable() -> Bool {
+    
+    /// Проверка доступности сети
+    /// - Returns: достпен/недоступен
+    func isNetworkAvailable() -> Bool {
         if let path = self.path {
-           if path.status == NWPath.Status.satisfied {
-            return true
-          }
+            if path.status == NWPath.Status.satisfied {
+                return true
+            }
         }
        return false
-   }
- }
+    }
+}
